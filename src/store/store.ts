80275e818b5a1cd19c2470a25bd04ddf0ce09c7e
aman-godara <amangodara.auth@gmail.com>
2024-10-17 11:44:43 +0530

    Add prettier

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,19 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
+
+import crewsReducer from './features/crews';
+import usersReducer from './features/users';
 
 export const makeStore = () => {
-  return configureStore({
-    reducer: {},
-  });
+    return configureStore({
+        reducer: {
+            crews: crewsReducer,
+            users: usersReducer,
+        },
+    });
 };
 
 // Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore["getState"]>;
-export type AppDispatch = AppStore["dispatch"];
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
