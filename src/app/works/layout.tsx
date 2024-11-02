ab89075522fb05332d8d0e6f829c038c1ae10c5b
Aman Godara <amangodara.auth@gmail.com>
2024-11-02 11:59:20 +0530

    Change title text

--- src/app/works/layout.tsx
+++ src/app/works/layout.tsx
@@ -80,7 +80,7 @@ const RightContent: React.FC<RightContentProps> = ({ children }) => {
         <div className="flex-1 flex flex-col">
             <div className="p-6">
                 <h1 className="text-2xl font-bold tracking-tight text-white">
-                    Works Dashboard
+                    Title of the work goes here
                 </h1>
             </div>
             <div className="px-6">
