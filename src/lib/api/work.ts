6f9990a6dda75e6d373a606baeed91d9c583fb88
Aman Godara <amangodara.auth@gmail.com>
2025-01-01 18:19:40 +0530

    Add get ancestors api

--- src/lib/api/work.ts
+++ src/lib/api/work.ts
@@ -1,24 +1,38 @@
-import { supabaseBrowserClient } from "@/lib/supabase/browser";
-
+import { supabaseBrowserClient } from '@/lib/supabase/browser';
 
 export async function getWorks(page: number = 1, limit: number = 10) {
     try {
         const from = (page - 1) * limit;
         const to = from + limit - 1;
-        
-        const countResponse = await supabaseBrowserClient.from('works')
+
+        const countResponse = await supabaseBrowserClient
+            .from('works')
             .select('*', { count: 'exact', head: true });
 
-        const totalPages = Math.ceil(countResponse.count as number / limit);
+        const totalPages = Math.ceil((countResponse.count as number) / limit);
 
-        const { data, error } = await supabaseBrowserClient.from('works')
+        const { data, error } = await supabaseBrowserClient
+            .from('works')
             .select('*')
             .range(from, to);
 
         if (error) throw error;
 
-        return { ok : true, status : 200, data: data, totalPages: totalPages };
+        return { ok: true, status: 200, data: data, totalPages: totalPages };
     } catch (error) {
         return { ok: false, status: 500, error: error };
     }
+}
+
+export async function getAncestors({
+    workId,
+    length,
+}: {
+    workId: string;
+    length: number;
+}) {
+    return await supabaseBrowserClient.rpc('get_ancestors', {
+        work_id: workId,
+        length: length,
+    });
 }
