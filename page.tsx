0e41e243f32e083713249acf65e689e79b94492e
Neeraj Rathor <157271977+Neerrr@users.noreply.github.com>
2025-01-12 18:47:38 +0530

    Add files via upload
    Forgot Password Page

--- page.tsx
+++ page.tsx
@@ -1,56 +1,56 @@
-'use client'
+'use client';
 
-import { logInAction } from "@/app/actions";
+import { forgotPasswordAction } from "@/app/actions";
 import Link from "next/link";
 import { FormEvent, useState } from "react";
 
-export default function Login() {
-    const [error, setError] = useState("");
+export default function ForgotPassword() {
+    const [message, setMessage] = useState("");
 
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
-        const response = await logInAction(formData);
+        const response = await forgotPasswordAction(formData);
 
-        if (!response.success) {
-            setError(response.message);
-        } else {
-            window.location.href = "/protected"; 
+        if (response.success) {
+            setMessage(response.message);
         }
     };
 
     return (
-        <div className="flex flex-col items-center justify-start min-h-screen mt-12">
-            {/* Login Form placed after the header */}
-            <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center w-full max-w-sm text-center">
-                <h1 className="text-2xl font-medium text-white">Sign in</h1>
-                <p className="text-sm text-white">
-                    Don&apos;t have an account?{" "}
-                    <Link className="text-white font-medium underline" href="/sign-up">
-                        Sign up
-                    </Link>
-                </p>
-                {error && <p className="text-red-500">{error}</p>}
-                <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-                    <label htmlFor="email" className="text-white">Email</label>
-                    <input className="text-black" name="email" placeholder="you@example.com" required />
-                    <div className="flex justify-between items-center">
-                        <label htmlFor="password" className="text-white">Password</label>
-                        <Link className="text-xs text-white underline" href="/forgot-password">
-                            Forgot Password?
+        <div className="flex justify-center items-start min-h-screen text-white mt-16">
+            <form
+                onSubmit={handleSubmit}
+                className="flex flex-col w-full gap-2 [&>input]:mb-6 max-w-64 mx-auto"
+            >
+                <div className="text-center">
+                    <h1 className="text-2xl font-bold">Reset Password</h1>
+                    <p className="text-sm mt-4">
+                        Already have an account?{" "}
+                        <Link
+                            className="text-white font-medium underline"
+                            href="/login"
+                        >
+                            Sign in
                         </Link>
-                    </div>
+                    </p>
+                </div>
+                <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+                    <label htmlFor="email">Email</label>
                     <input
                         className="text-black"
-                        type="password"
-                        name="password"
-                        placeholder="Your password"
+                        name="email"
+                        placeholder="you@example.com"
                         required
                     />
-                    <button className="mt-4 py-1 px-6 bg-white text-black font-bold rounded-lg" type="submit">
-                        Sign in
+                    <button
+                        type="submit"
+                        className="mt-4 py-1.5 px-6 bg-white text-black font-bold rounded-lg"
+                    >
+                        Reset Password
                     </button>
                 </div>
+                <span className="mt-10">{message}</span>
             </form>
         </div>
     );
