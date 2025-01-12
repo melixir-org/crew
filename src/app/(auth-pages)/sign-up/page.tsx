20c2e68b544b9e2819d0f9d8260a8daacd6dfb34
Neeraj Rathor <157271977+Neerrr@users.noreply.github.com>
2025-01-12 20:59:40 +0530

    Update page.tsx
    corrected the alignment and the color of text on the sign up screen and additionally created the create account button on the sign up screen

--- src/app/(auth-pages)/sign-up/page.tsx
+++ src/app/(auth-pages)/sign-up/page.tsx
@@ -3,26 +3,26 @@ import Link from 'next/link';
 
 export default function Signup() {
     return (
-        <form className="flex flex-col min-w-64 max-w-64 mx-auto">
-            <h1 className="text-2xl font-medium">Sign up</h1>
-            <p className="text-sm text text-foreground">
+        <form className="flex flex-col min-w-64 max-w-64 mx-auto mt-12"> 
+            <h1 className="text-2xl font-medium text-white text-center">Sign up</h1> {/* Centered only this */}
+            <p className="text-sm text-white text-center">
                 Already have an account?{' '}
                 <Link
-                    className="text-primary font-medium underline"
+                    className="text-white font-medium underline"
                     href="/login"
                 >
                     Sign in
                 </Link>
             </p>
             <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-                <label htmlFor="email">Email</label>
+                <label htmlFor="email" className="text-white">Email</label>
                 <input
                     className="text-black"
                     name="email"
                     placeholder="you@example.com"
                     required
                 />
-                <label htmlFor="password">Password</label>
+                <label htmlFor="password" className="text-white">Password</label>
                 <input
                     type="password"
                     name="password"
@@ -31,7 +31,9 @@ export default function Signup() {
                     required
                     className="text-black"
                 />
-                {/* <button formAction={signUpAction}>Sign up</button> */}
+                <button type="submit" className="mt-6 py-1.5 px-6 bg-white text-black rounded-lg font-bold">
+                    Create Account
+                </button>
             </div>
         </form>
     );
