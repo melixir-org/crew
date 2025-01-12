873e27b0db4164253287186e25f5ce6322b6fa01
Neeraj Rathor <157271977+Neerrr@users.noreply.github.com>
2025-01-12 18:33:54 +0530

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,40 +1,57 @@
-import { signUpAction } from '@/app/actions';
-import Link from 'next/link';
+'use client';
 
-export default function Signup() {
+import { forgotPasswordAction } from "@/app/actions";
+import Link from "next/link";
+import { FormEvent, useState } from "react";
+
+export default function ForgotPassword() {
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const response = await forgotPasswordAction(formData);
+
+        if (response.success) {
+            setMessage(response.message);
+        }
+    };
+
     return (
-        <form className="flex flex-col min-w-64 max-w-64 mx-auto mt-12">  {/* Adjusted mt-12 to match the login screen */}
-            <h1 className="text-2xl font-medium text-white text-center">Sign up</h1> {/* Centered only this */}
-            <p className="text-sm text-white text-center">
-                Already have an account?{' '}
-                <Link
-                    className="text-white font-medium underline"
-                    href="/login"
-                >
-                    Sign in
-                </Link>
-            </p>
-            <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-                <label htmlFor="email" className="text-white">Email</label>
-                <input
-                    className="text-black"
-                    name="email"
-                    placeholder="you@example.com"
-                    required
-                />
-                <label htmlFor="password" className="text-white">Password</label>
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Your password"
-                    minLength={6}
-                    required
-                    className="text-black"
-                />
-                <button type="submit" className="mt-6 py-1.5 px-6 bg-white text-black rounded-lg font-bold">
-                    Create Account
-                </button>
-            </div>
-        </form>
+        <div className="flex justify-center items-start min-h-screen text-white mt-16">
+            <form
+                onSubmit={handleSubmit}
+                className="flex flex-col w-full gap-2 [&>input]:mb-6 max-w-64 mx-auto"
+            >
+                <div className="text-center">
+                    <h1 className="text-2xl font-bold">Reset Password</h1>
+                    <p className="text-sm mt-4">
+                        Already have an account?{" "}
+                        <Link
+                            className="text-white font-medium underline"
+                            href="/login"
+                        >
+                            Sign in
+                        </Link>
+                    </p>
+                </div>
+                <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        className="text-black"
+                        name="email"
+                        placeholder="you@example.com"
+                        required
+                    />
+                    <button
+                        type="submit"
+                        className="mt-4 py-1.5 px-6 bg-white text-black font-bold rounded-lg"
+                    >
+                        Reset Password
+                    </button>
+                </div>
+                <span className="mt-10">{message}</span>
+            </form>
+        </div>
     );
 }
