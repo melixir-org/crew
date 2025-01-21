28c5f46a36d47a7dbc9e85ed3d0931289ad32fa3
rishabh-awa <awasthirishabh52@gmail.com>
2025-01-21 17:14:39 +0530

    Unassigned Done

--- src/components/custom/WorkHome/AssignmentCard.tsx
+++ src/components/custom/WorkHome/AssignmentCard.tsx
@@ -1,12 +1,31 @@
-import { getRelativeTime } from '@/lib/utils';
+import { unassignApi } from '@/lib/client-only-api';
+import { extractWorkId, getFormattedTime, getRelativeTime } from '@/lib/utils';
+import { usePageStore } from '@/provider/PageStore';
 import { Assignment } from '@/types/Assignment';
+import { usePathname } from 'next/navigation';
 
 interface AssignmentCardProps {
     data: Assignment;
 }
 
 const AssignmentCard: React.FC<AssignmentCardProps> = ({ data }) => {
+    const pathname = usePathname();
+    const workId: string = extractWorkId(pathname);
+
+    const { setWork } = usePageStore(store => store);
+
     const time = getRelativeTime(data.assigned_at);
+
+    const unassign = async () => {
+        const assingment_id = data.id;
+        const currentTime = getFormattedTime();
+        await unassignApi(assingment_id, currentTime);
+        setWork(workId, work => {
+            work.assignment = work.assignment?.filter(
+                assignment => assignment.unassigned_at !== null
+            );
+        });
+    };
 
     return (
         <div className="w-full p-3 bg-secondary-dark-bg border-[1px] border-dark-border rounded-lg flex flex-col gap-1 mt-1">
@@ -17,6 +36,12 @@ const AssignmentCard: React.FC<AssignmentCardProps> = ({ data }) => {
                 </h6>
             </div>
             <div className="name">{data.user_id}</div>
+            <button
+                className="border-[1px] border-dark-border text-xs font-medium py-[1px] px-2 rounded-md"
+                onClick={unassign}
+            >
+                Unassign
+            </button>
         </div>
     );
 };
--- src/types/Assignment.ts
+++ src/types/Assignment.ts
@@ -4,12 +4,14 @@ export interface Assignment {
     id: string;
     user_id: string;
     assigned_at: string;
+    unassigned_at: string;
 }
 
 export function createAssignment({
     id = '',
     user_id = '',
     assigned_at = '',
+    unassigned_at = '',
 }: DeepPartial<Assignment> = {}): Assignment {
-    return { id, user_id, assigned_at };
+    return { id, user_id, assigned_at, unassigned_at };
 }
