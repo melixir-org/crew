94c44ca6a1946a41a8f47094f72fa04b3499eba7
Aman Godara <amangodara.auth@gmail.com>
2025-04-08 14:42:52 +0530

    Show avatar when logged in

--- src/components/custom/NavBar.tsx
+++ src/components/custom/NavBar.tsx
@@ -8,8 +8,14 @@ import {
 } from '../ui/navigation-menu';
 import { HOME_ROUTE, AUTH_ROUTE, WORKSPACE_ROUTE } from '@/app/routes';
 import { cn } from '@/lib/utils';
+import { getUserApi } from '@/lib/server-only-api';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 
-const NavBar = () => {
+const NavBar = async () => {
+    const {
+        data: { user },
+    } = await getUserApi();
+
     return (
         <div className="flex justify-between px-12 py-2 border-b border-zinc-800 bg-primary-dark-bg backdrop-blur supports-[backdrop-filter]:bg-black/60">
             <NavigationMenu>
@@ -27,12 +33,39 @@ const NavBar = () => {
             <NavigationMenu>
                 <NavigationMenuList>
                     <NavigationMenuItem>
-                        <Link
-                            href={AUTH_ROUTE.pathname}
-                            className={cn(navigationMenuTriggerStyle(), 'h-8')}
-                        >
-                            <div>Login or Sign Up</div>
-                        </Link>
+                        {user ? (
+                            <Link href={AUTH_ROUTE.pathname}>
+                                <Avatar className="h-8 w-8">
+                                    {user.user_metadata.avatar_url ? (
+                                        <AvatarImage
+                                            src={user.user_metadata.avatar_url}
+                                        />
+                                    ) : (
+                                        <AvatarFallback className="bg-gray-100 text-black">
+                                            <span className="text-xs">
+                                                {user.user_metadata.name
+                                                    .split(' ')
+                                                    .map((t: string) =>
+                                                        t[0].toUpperCase()
+                                                    )
+                                                    .join('')
+                                                    .slice(0, 2)}
+                                            </span>
+                                        </AvatarFallback>
+                                    )}
+                                </Avatar>
+                            </Link>
+                        ) : (
+                            <Link
+                                href={AUTH_ROUTE.pathname}
+                                className={cn(
+                                    navigationMenuTriggerStyle(),
+                                    'h-8'
+                                )}
+                            >
+                                <div>Login or Sign Up</div>
+                            </Link>
+                        )}
                     </NavigationMenuItem>
                 </NavigationMenuList>
             </NavigationMenu>
